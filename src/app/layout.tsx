e32e9a5ebf2977af67730d6f4f616e3e915429ce
pimentellima <matheus_dere@hotmail.com>
2024-01-14 20:55:06 -0300

    responsividade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,54 +24,73 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const user = await currentUser();
-  if (!user)
-    return (
-      <ClerkProvider>
-        <html lang="en">
-          <Analytics />
-          <body className={inter.className}>
-            <header className="grid grid-cols-3 sticky px-16 top-0 w-full h-12 
-            bg-white dark:bg-stone-700 shadow-md">
-              <div className="flex items-center justify-center gap-20 col-start-2">
-                <Link href={"/search"} title="Search users">
-                  <MagnifyingGlassIcon className="h-6 w-6" />
-                </Link>
-                <div className="h-6 w-6">
-                  <Link title="Sign in" href={"/"}>
-                    <UserCircleIcon className="h-6 w-6" />
-                  </Link>
-                </div>
-              </div>
-            </header>
-            <div className="pt-6">{children}</div>
-          </body>
-        </html>
-      </ClerkProvider>
-    );
 
   return (
     <ClerkProvider>
       <html lang="en">
         <Analytics />
         <body className={inter.className}>
-          <header className="grid grid-cols-3 sticky px-16 top-0 w-full h-12 
-          bg-white dark:bg-stone-700 shadow-md">
-            <div className="flex items-center justify-between col-start-2">
+          <header
+            className="xl:hidden flex justify-around items-center sticky top-0 w-full h-12 
+          bg-white dark:bg-stone-700 shadow-md"
+          >
+            <Link href={"/search"} title="Search users">
+              <MagnifyingGlassIcon className="h-6 w-6" />
+            </Link>
+            {!!user && (
+              <>
+                <Link href={"/inbox"} title="Inbox">
+                  <InboxIcon className="h-6 w-6" />
+                </Link>
+                <Link href={"/notifications"} title="Notifications">
+                  <BellIcon className="h-6 w-6" />
+                </Link>
+              </>
+            )}
+            {!!user ? (
+              <div className="h-6 w-6 flex items-center">
+                <UserButton afterSignOutUrl="/" />
+              </div>
+            ) : (
+              <div className="h-6 w-6 flex items-center ">
+                <Link title="Sign in" href={"/"}>
+                  <UserCircleIcon className="h-6 w-6" />
+                </Link>
+              </div>
+            )}
+          </header>
+          <header
+            className="hidden xl:grid grid-cols-3 sticky px-16 top-0 w-full h-12 
+          bg-white dark:bg-stone-700 shadow-md"
+          >
+            <div className="flex items-center justify-around col-start-2">
               <Link href={"/search"} title="Search users">
                 <MagnifyingGlassIcon className="h-6 w-6" />
               </Link>
-              <Link href={"/inbox"} title="Inbox">
-                <InboxIcon className="h-6 w-6" />
-              </Link>
-              <Link href={"/notifications"} title="Notifications">
-                <BellIcon className="h-6 w-6" />
-              </Link>
-              <div className="h-6 w-6">
-                <UserButton />
-              </div>
+              {!!user && (
+                <>
+                  <Link href={"/inbox"} title="Inbox">
+                    <InboxIcon className="h-6 w-6" />
+                  </Link>
+                  <Link href={"/notifications"} title="Notifications">
+                    <BellIcon className="h-6 w-6" />
+                  </Link>
+                </>
+              )}
+              {!!user ? (
+                <div className="h-6 w-6 flex items-center">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+              ) : (
+                <div className="h-6 w-6 flex items-center">
+                  <Link title="Sign in" href={"/"}>
+                    <UserCircleIcon className="h-6 w-6" />
+                  </Link>
+                </div>
+              )}
             </div>
           </header>
-          <div className="pt-6 pb-2">{children}</div>
+          <div className="pt-3 xl:pt-6 pb-2 xl:px-0 px-2">{children}</div>
         </body>
       </html>
     </ClerkProvider>
