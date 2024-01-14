89534b8d6d39c3227d96d4943ff2c4765a2eb4e2
pimentellima <matheus_dere@hotmail.com>
2024-01-14 00:06:38 -0300

    analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Analytics />
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
